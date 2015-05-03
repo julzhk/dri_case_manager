d815dd27be67adcb07d8beda7ebe124c4e3e8da6
Julian Harley <julian.harley@pancentric.com>
2015-05-03 12:59:10 +0100

    refactor js into a file

---
+++ www/js/app.js
@@ -0,0 +1,47 @@
+function write_nav_to_ui(limit,skip, page, count){
+    $('.nav').append('Records '+ (skip) + ' to '+ (skip + limit) + ' of '+ count + '<br>');
+    if(page>1){
+        $('.nav').append('<a href="?p='+ (page-1) + '">Newer</a> | ');
+    }
+    if((page+1)*limit<count){
+        $('.nav').append('<a href="?p='+ (page+1) + '">Older</a>');
+    }
+}
+
+
+function write_to_ui(rows){
+    _.each(rows,function(row){
+        $('.data').append("<tr>");
+        $('.data').append("<td>"+ row.id+"</td>");
+        $('.data').append("<td>"+ row.doc.title+"</td>");
+        $('.data').append("</tr>");
+        console.log(row);
+    });
+}
+
+
+function showrecords(limit,skip, page) {
+    db.allDocs({
+        include_docs: true,
+        descending: true,
+        limit:limit,
+        skip:skip
+    }, function(err, doc) {
+        console.log(doc);
+        write_to_ui(doc.rows);
+        write_nav_to_ui(limit,skip, page, doc.total_rows);
+    });
+}
+
+
+$.urlParam = function(name){
+    //   extract parameters from url:
+    //   http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
