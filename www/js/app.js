48b324a5ce177cdeda85619fa94b0e54e1bfd60c
Julian Harley <julian.harley@pancentric.com>
2015-05-04 11:16:45 +0100

    refactor & add patient number hidden field to social form

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,7 @@ var db = new PouchDB('http://localhost:5984/my_database');
 var syncDom = document.getElementById('sync-wrapper');
 
 function write_nav_to_ui(limit,skip, page, count){
+    // used only on index.html
     $('.nav').append('Records '+ (skip) + ' to '+ (skip + limit) + ' of '+ count + '<br>');
     if(page>1){
         $('.nav').append('<a href="?p='+ (page-1) + '">Newer</a> | ');
@@ -15,13 +16,14 @@ function write_nav_to_ui(limit,skip, page, count){
 
 
 function write_to_ui(rows){
+    // used only on index.html
     _.each(rows,function(row){
         $('.data').append("<tr class='table-striped'>");
-        //date
+        //columns:date
         $('.data').append("<td>"+ row.id+"</td>");
-        // number
+        //columns: number
         $('.data').append("<td>"+ row.id+"</td>");
-        // name
+        //columns: name
         $('.data').append("<td>"+ row.doc.patientName+"</td>");
         $('.data').append("</tr>");
         console.log(row);
@@ -30,6 +32,7 @@ function write_to_ui(rows){
 
 
 function showrecords(limit,skip, page) {
+    // used only on index.html
     db.allDocs({
         include_docs: true,
         descending: true,
@@ -39,8 +42,12 @@ function showrecords(limit,skip, page) {
         if (typeof doc === "undefined") {
             console.log('no records')
         } else {
-            write_to_ui(doc.rows);
-            write_nav_to_ui(limit,skip, page, doc.total_rows);
+            if (err==null){
+                write_to_ui(doc.rows);
+                write_nav_to_ui(limit,skip, page, doc.total_rows);
+            } else{
+                db_error(err);
+            }
         }
     });
 }
@@ -58,13 +65,15 @@ $.urlParam = function(name){
     }
 }
 
-// There was some form or error syncing
+
 function syncError() {
-    syncDom.setAttribute('data-sync-state', 'error');
+    // There was some form or error syncing
+    //todo write to DOM
+    console.log('omg sync fail')
 }
 
 function sync() {
-//            syncDom.setAttribute('data-sync-state', 'syncing');
+    // todo: write: syncing to DOM
     var opts = {live: true};
     db.replicate.to(db, opts, syncError);
     db.replicate.from(db, opts, syncError);
@@ -88,17 +97,29 @@ function add_new_record(event){
     db.put(new_record, function callback(err, result) {
         if (!err) {
             console.log('Successfully posted a new_record!');
+        } else {
+            db_error(err);
         }
     });
     sync();
+    // we use ID to JOIN all other records to this master record.
+    // gets passed from page to page in the ID cookie.
     Cookies.set('id', id);
     window.location.href = "admin.html";
     event.preventDefault();
 }
 
+function db_error(err){
+    //todo : write to DOM
+    console.log(err);
+}
 
 function save_on_page_change(event){
-    console.log('93 save_on_page_change');
+    // each form is a new document (key by save datetime.
+    // todo: allow the key instead; FYI: must update all fields,
+    // partial updates not possible
+    var id = new Date().toISOString();
+    // update with var id = Cookies.get('id');
     var data = $( "form" ).serializeArray();
     var data_dict = {};
         _.each(data,function(ele){
@@ -107,9 +128,7 @@ function save_on_page_change(event){
     console.log(data);
     console.log(data_dict);
     var destination_url = event.currentTarget.href;
-    var id = Cookies.get('id');
     sync();
-
     db.get(id).then(function(doc) {
         data_dict._id=id;
         data_dict._rev=doc._rev;
@@ -118,6 +137,7 @@ function save_on_page_change(event){
     }).then(function(response) {
       // handle response
     }).catch(function (err) {
+        db_error(err);
       console.log(err);
     });
 
